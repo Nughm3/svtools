0a73c6ab5eb309cf05cd099387f4716fe30d3fc3
Isaac Hung <manhei5001@gmail.com>
2022-08-06 22:42:14 +0800

    refactor

---
+++ sv-list/src/lib.rs
@@ -0,0 +1,93 @@
+use colored::Colorize;
+
+pub fn fmt(input: &str) -> String {
+    if input.contains("access denied") {
+        format!("{}", "[access denied]".red(),)
+    } else {
+        let input: String = input.split(';').take(1).collect();
+
+        let status = if input.contains("run:") {
+            format!("{}", "up".green())
+        } else {
+            format!("{}", "down".red())
+        };
+
+        let time = secs_fmt(if status.contains("up") {
+            input
+                .split_whitespace()
+                .last()
+                .expect("Time is present")
+                .replace('s', "")
+                .parse()
+                .expect("Time is a valid `u64` value")
+        } else {
+            input
+                .split_whitespace()
+                .nth(2)
+                .expect("Time is present")
+                .replace("s,", "")
+                .parse()
+                .expect("Time is a valid `u64` value")
+        });
+
+        let pid = if status.contains("up") {
+            format!(
+                "PID {}",
+                input
+                    .split("(pid ")
+                    .last()
+                    .unwrap()
+                    .split_whitespace()
+                    .next()
+                    .unwrap()
+                    .replace(')', "")
+            )
+        } else {
+            "".to_owned()
+        };
+
+        format!(
+            "{} {} {} {} {} {}",
+            "[".yellow(),
+            status.yellow(),
+            "for".blue(),
+            time.blue(),
+            "]\t".yellow(),
+            pid.red()
+        )
+    }
+}
+
+fn secs_fmt(s: u64) -> String {
+    let hours = s / 3600;
+    let mins = s % 3600 / 60;
+    let secs = s % 3600 % 60;
+
+    let mut s = String::new();
+    if hours > 0 {
+        s.push_str(&format!("{}h", hours));
+    }
+    if mins > 0 {
+        s.push_str(&format!("{}m", mins));
+    }
+    if secs > 0 {
+        s.push_str(&format!("{}s", secs));
+    }
+    s
+}
+
+#[cfg(test)]
+mod tests {
+    use super::secs_fmt;
+
+    #[test]
+    fn fmt_1() {
+        let time = secs_fmt(3880);
+        assert_eq!(time, String::from("1h4m40s"));
+    }
+    #[test]
+    fn fmt_2() {
+        let time = secs_fmt(628);
+        assert_eq!(time, String::from("10m28s"));
+    }
+}
--- sv-list/src/main.rs
+++ sv-list/src/main.rs
@@ -1,16 +1,10 @@
-use color_eyre::Result;
 use colored::Colorize;
-use std::{env, fs, process::Command, str::from_utf8};
+use std::{error::Error, fs, process::Command, str::from_utf8};
 
 const RUNSVDIR: &str = "/var/service/";
 const SVDIR: &str = "/etc/sv/";
 
-fn main() -> Result<()> {
-    if env::var("RUST_LIB_BACKTRACE").is_err() {
-        env::set_var("RUST_LIB_BACKTRACE", "1");
-    }
-    color_eyre::install()?;
-
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Service Overview");
 
     let mut enabled: Vec<_> = fs::read_dir(RUNSVDIR)?
@@ -33,7 +27,7 @@ fn main() -> Result<()> {
         .map(|x| x.to_str().expect("Failed to convert string"))
     {
         let status = Command::new("sv").arg("status").arg(svc).output()?;
-        let status = fmt(from_utf8(&status.stdout)?.trim_end());
+        let status = sv_list::fmt(from_utf8(&status.stdout)?.trim_end());
 
         if status.contains("up") {
             up += 1;
@@ -48,105 +42,9 @@ fn main() -> Result<()> {
     {
         println!("{} {}", "[ ] [ disabled ]".red(), svc.red());
     }
-    println!(
-        "{}/{} enabled, {}/{} up",
-        enabled.len(),
-        enabled.len() + disabled.len(),
-        up,
-        enabled.len(),
-    );
+
+    let (en, dis) = (enabled.len(), disabled.len());
+    println!("{en}/{} enabled, {}/{} up", en + dis, up, en);
 
     Ok(())
-}
-
-fn fmt(input: &str) -> String {
-    if input.contains("access denied") {
-        format!("{}", "[access denied]".red(),)
-    } else {
-        let input: String = input.split(';').take(1).collect();
-
-        let status = if input.contains("run:") {
-            format!("{}", "up".green())
-        } else {
-            format!("{}", "down".red())
-        };
-
-        let time = secs_fmt(if status.contains("up") {
-            input
-                .split_whitespace() // The last element will be the time
-                .last()
-                .expect("Will never fail")
-                .replace('s', "")
-                .parse() // turn it into a u64
-                .expect("Will never fail")
-        } else {
-            input
-                .split_whitespace()
-                .nth(2)
-                .expect("Will never fail")
-                .replace("s,", "")
-                .parse() // turn it into a u64
-                .expect("Will never fail")
-        });
-
-        let pid = if status.contains("up") {
-            format!(
-                "PID {}",
-                input
-                    .split("(pid ")
-                    .last()
-                    .expect("Will never fail")
-                    .split_whitespace()
-                    .next()
-                    .unwrap()
-                    .replace(')', "")
-            )
-        } else {
-            "".to_owned()
-        };
-
-        format!(
-            "{} {} {} {} {} {}",
-            "[".yellow(),
-            status.yellow(),
-            "for".blue(),
-            time.blue(),
-            "]\t".yellow(),
-            pid.red()
-        )
-    }
-}
-
-fn secs_fmt(s: u64) -> String {
-    let hours = s / 3600;
-    let mins = s % 3600 / 60;
-    let secs = s % 3600 % 60;
-
-    let mut s = String::new();
-    if hours > 0 {
-        s.push_str(&format!("{}h", hours));
-    }
-    if mins > 0 {
-        s.push_str(&format!("{}m", mins));
-    }
-    if secs > 0 {
-        s.push_str(&format!("{}s", secs));
-    }
-    s
-}
-
-#[cfg(test)]
-mod tests {
-    use super::secs_fmt;
-
-    #[test]
-    fn fmt_1() {
-        let time = secs_fmt(3880);
-        assert_eq!(time, String::from("1h4m40s"));
-    }
-    #[test]
-    fn fmt_2() {
-        let time = secs_fmt(628);
-        assert_eq!(time, String::from("10m28s"));
-    }
 }
